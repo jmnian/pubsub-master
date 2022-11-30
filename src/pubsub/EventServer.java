2228a030e4ba360a56d4a1997c8cbc17ef5e7c63
Orange-C <oraaangecolin@gmail.com>
2022-11-29 20:42:49 -0800

    Add switch server

--- src/pubsub/EventServer.java
+++ src/pubsub/EventServer.java
@@ -7,7 +7,6 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
--- src/pubsub/PubSubClient.java
+++ src/pubsub/PubSubClient.java
@@ -4,6 +4,7 @@ import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Scanner;
+import java.util.Set;
 
 import pubsub.interfaces.EventClient;
 import pubsub.interfaces.EventManager;
@@ -12,11 +13,13 @@ public class PubSubClient extends UnicastRemoteObject implements EventClient {
 
     private int id = 0;
 	private EventManager manager;
+	private EventManager failureDetector;
     
     public PubSubClient(int id, int port) throws RemoteException {
 		this.id = id;
 
     	try {
+			this.failureDetector = (EventManager) Naming.lookup("//127.0.0.1:6666/FailureDetector");
 			this.manager = (EventManager) Naming.lookup("//localhost:" + port + "/EventServer");
 			System.out.println("Connected to server at " + port);
 		} catch (Exception e) {
@@ -24,8 +27,36 @@ public class PubSubClient extends UnicastRemoteObject implements EventClient {
 			System.exit(1);
 		}
     }
+
+	public void runFailuerDetector() throws Exception {
+		while (true) {
+			try {
+				this.manager.getRegisteredName();
+			} catch (RemoteException e) {
+				System.out.println("\nServer just went down");
+				Set<String> serverPool = this.failureDetector.getServerPool();
+
+				for(String s : serverPool) {
+					try {
+						this.manager = (EventManager) Naming.lookup(s);
+						System.out.println("Switch to " + s);
+						break;
+					} catch (Exception ee) {
+						System.out.println(ee);
+					}
+				}
+
+				if(this.manager == null) {
+					System.out.println("All server down");
+					System.exit(1);
+				}
+			}
+            
+            Thread.sleep(1000);
+        }
+	}
     
-	public static void main(String[] args) throws RemoteException {
+	public static void main(String[] args) throws Exception {
 		int id = -1;
      	int port = 1099;
 
@@ -50,6 +81,7 @@ public class PubSubClient extends UnicastRemoteObject implements EventClient {
 		PubSubClient client = new PubSubClient(id, port);
 		client.manager.addClient(id, client, true);
 		client.commandLineInterface();
+		client.runFailuerDetector();
 	}
 	
 	private void subscribe(String topic) throws RemoteException {
@@ -81,54 +113,65 @@ public class PubSubClient extends UnicastRemoteObject implements EventClient {
 	}
 	
 	public void commandLineInterface() throws RemoteException {
-		Scanner in = new Scanner(System.in);
+		Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+				Scanner in = new Scanner(System.in);
 
-		boolean continueExec = true;
-		while (continueExec) {
-			System.out.println("Client actions [1-5]:");
-			System.out.println(" 1: Create a topic");
-			System.out.println(" 2: Publish an event");
-			System.out.println(" 3: Subscribe to a topic");
-			System.out.println(" 4: Unsubscribe from a topic");
-			System.out.println(" 5: Go back");
-			System.out.print(": ");
-			int choice = -1;
+				while (true) {
+					System.out.println("Client actions [1-5]:");
+					System.out.println(" 1: Create a topic");
+					System.out.println(" 2: Publish an event");
+					System.out.println(" 3: Subscribe to a topic");
+					System.out.println(" 4: Unsubscribe from a topic");
+					System.out.println(" 5: Go back");
+					System.out.print(": ");
+					int choice = -1;
 
-			try {
-				choice = in.nextInt(); 
-				in.nextLine();
-			} catch (Exception e) { 
-				in.nextLine(); 
+					try {
+						choice = in.nextInt(); 
+						in.nextLine();
+					} catch (Exception e) { 
+						in.nextLine(); 
+					}
+
+					try {
+						switch (choice) {
+							case 1: 
+								System.out.println("Enter topic name:");
+								createTopic(in.nextLine().trim()); 
+								break;
+							case 2: 
+								System.out.println("Enter topic name:"); 
+								String topic = in.nextLine().trim();
+								System.out.println("Enter the message:"); 
+								publish(topic, in.nextLine().trim()); 
+								break;
+							case 3: 
+								System.out.println("Enter topic name:"); 
+								subscribe(in.nextLine().trim());
+								break;
+							case 4:
+								System.out.println("Enter topic name:"); 
+								unsubscribe(in.nextLine().trim());
+								break;
+							case 5: 
+								in.close();
+								System.exit(1);
+								break;
+							default: 
+								System.out.println("Input not recognized");
+						}
+					} catch (RemoteException e) {
+						e.printStackTrace();
+					}
+				}
 			}
-
-			switch (choice) {
-				case 1: 
-					System.out.println("Enter topic name:");
-					createTopic(in.nextLine().trim()); 
-					break;
-				case 2: 
-					System.out.println("Enter topic name:"); 
-					String topic = in.nextLine().trim();
-					System.out.println("Enter the message:"); 
-					publish(topic, in.nextLine().trim()); 
-					break;
-				case 3: 
-					System.out.println("Enter topic name:"); 
-					subscribe(in.nextLine().trim());
-					break;
-				case 4:
-					System.out.println("Enter topic name:"); 
-					unsubscribe(in.nextLine().trim());
-					break;
-				case 5: 
-					continueExec = false; 
-					break;
-				default: 
-					System.out.println("Input not recognized");
-			}
-		}
-		in.close();
-		System.exit(1);
+		});
+		// Daemon allows this thread not to block program from exiting
+		t.setDaemon(true);
+		t.start();		
+		
 	}
 
 	public void notify(String topic, String msg) {
